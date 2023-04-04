94bf5aabfb9191e1c4f81d90488549048c3a6e1c
David Leonardo Acosta Laverde <david.acosta.ing@gmail.com>
2023-04-04 08:47:03 -0500

    Score system

---
+++ Assets/Scripts/Enemies/EnemiesManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Enemies;
+using UnityEngine;
+
+public class EnemiesManager : MonoBehaviour
+{
+    [SerializeField] private List<BaseEnemy> enemies;
+
+    public int GetEnemies()
+    {
+        return enemies.Count;
+    }
+}
